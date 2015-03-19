48022531f8dedfdb49e1226fc8101aaa97d37980
Karim <unbelt@outlook.com>
2015-03-19 20:59:26 +0200

    Migrations update

---
+++ Supermarket.Data/Migrations/201503191332282_Oracle.cs
@@ -0,0 +1,91 @@
+/*
+namespace Supermarket.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Oracle : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "HERCULES.MEASURES",
+                c => new
+                    {
+                        MEASURE_ID = c.Decimal(nullable: false, precision: 10, scale: 0, identity: true),
+                        MEASURE_NAME = c.String(nullable: false, maxLength: 20),
+                    })
+                .PrimaryKey(t => t.MEASURE_ID);
+            
+            CreateTable(
+                "HERCULES.PRODUCTS",
+                c => new
+                    {
+                        PRODUCT_ID = c.Decimal(nullable: false, precision: 10, scale: 0, identity: true),
+                        PRODUCT_NAME = c.String(nullable: false, maxLength: 50),
+                        PRODUCT_PRICE = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        MEASURE_ID = c.Decimal(nullable: false, precision: 10, scale: 0),
+                        VENDOR_ID = c.Decimal(nullable: false, precision: 10, scale: 0),
+                    })
+                .PrimaryKey(t => t.PRODUCT_ID)
+                .ForeignKey("HERCULES.MEASURES", t => t.MEASURE_ID, cascadeDelete: true)
+                .ForeignKey("HERCULES.VENDORS", t => t.VENDOR_ID, cascadeDelete: true)
+                .Index(t => t.MEASURE_ID)
+                .Index(t => t.VENDOR_ID);
+            
+            CreateTable(
+                "HERCULES.VENDORS",
+                c => new
+                    {
+                        VENDOR_ID = c.Decimal(nullable: false, precision: 10, scale: 0, identity: true),
+                        VENDOR_NAME = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.VENDOR_ID);
+            
+            CreateTable(
+                "HERCULES.SALES",
+                c => new
+                    {
+                        SALE_ID = c.Decimal(nullable: false, precision: 10, scale: 0, identity: true),
+                        SUPERMARKET_ID = c.Decimal(nullable: false, precision: 10, scale: 0),
+                        PRODUCT_ID = c.Decimal(nullable: false, precision: 10, scale: 0),
+                        QUANTITY = c.Decimal(nullable: false, precision: 19, scale: 0),
+                        UNIT_PRICE = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        SALE_SUM = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.SALE_ID)
+                .ForeignKey("HERCULES.PRODUCTS", t => t.PRODUCT_ID, cascadeDelete: true)
+                .ForeignKey("HERCULES.SUPERMARKETS", t => t.SUPERMARKET_ID, cascadeDelete: true)
+                .Index(t => t.SUPERMARKET_ID)
+                .Index(t => t.PRODUCT_ID);
+            
+            CreateTable(
+                "HERCULES.SUPERMARKETS",
+                c => new
+                    {
+                        SUPERMARKET_ID = c.Decimal(nullable: false, precision: 10, scale: 0, identity: true),
+                        SUPERMARKET_NAME = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.SUPERMARKET_ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("HERCULES.SALES", "SUPERMARKET_ID", "HERCULES.SUPERMARKETS");
+            DropForeignKey("HERCULES.SALES", "PRODUCT_ID", "HERCULES.PRODUCTS");
+            DropForeignKey("HERCULES.PRODUCTS", "VENDOR_ID", "HERCULES.VENDORS");
+            DropForeignKey("HERCULES.PRODUCTS", "MEASURE_ID", "HERCULES.MEASURES");
+            DropIndex("HERCULES.SALES", new[] { "PRODUCT_ID" });
+            DropIndex("HERCULES.SALES", new[] { "SUPERMARKET_ID" });
+            DropIndex("HERCULES.PRODUCTS", new[] { "VENDOR_ID" });
+            DropIndex("HERCULES.PRODUCTS", new[] { "MEASURE_ID" });
+            DropTable("HERCULES.SUPERMARKETS");
+            DropTable("HERCULES.SALES");
+            DropTable("HERCULES.VENDORS");
+            DropTable("HERCULES.PRODUCTS");
+            DropTable("HERCULES.MEASURES");
+        }
+    }
+}
+*/
---
+++ Supermarket.Data/Migrations/201503191334465_Sql.cs
@@ -0,0 +1,89 @@
+namespace Supermarket.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Sql : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Measures",
+                c => new
+                    {
+                        MeasureId = c.Int(nullable: false, identity: true),
+                        MeasureName = c.String(nullable: false, maxLength: 20),
+                    })
+                .PrimaryKey(t => t.MeasureId);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        ProductName = c.String(nullable: false, maxLength: 50),
+                        ProductPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        MeasureId = c.Int(nullable: false),
+                        VendorId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductId)
+                .ForeignKey("dbo.Measures", t => t.MeasureId, cascadeDelete: true)
+                .ForeignKey("dbo.Vendors", t => t.VendorId, cascadeDelete: true)
+                .Index(t => t.MeasureId)
+                .Index(t => t.VendorId);
+            
+            CreateTable(
+                "dbo.Vendors",
+                c => new
+                    {
+                        VendorId = c.Int(nullable: false, identity: true),
+                        VendorName = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.VendorId);
+            
+            CreateTable(
+                "dbo.Sales",
+                c => new
+                    {
+                        SaleId = c.Int(nullable: false, identity: true),
+                        SupermarketId = c.Int(nullable: false),
+                        ProductId = c.Int(nullable: false),
+                        Quantity = c.Long(nullable: false),
+                        UnitPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        SaleSum = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.SaleId)
+                .ForeignKey("dbo.Products", t => t.ProductId, cascadeDelete: true)
+                .ForeignKey("dbo.Supermarkets", t => t.SupermarketId, cascadeDelete: true)
+                .Index(t => t.SupermarketId)
+                .Index(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.Supermarkets",
+                c => new
+                    {
+                        SupermarketId = c.Int(nullable: false, identity: true),
+                        SupermarketName = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.SupermarketId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Sales", "SupermarketId", "dbo.Supermarkets");
+            DropForeignKey("dbo.Sales", "ProductId", "dbo.Products");
+            DropForeignKey("dbo.Products", "VendorId", "dbo.Vendors");
+            DropForeignKey("dbo.Products", "MeasureId", "dbo.Measures");
+            DropIndex("dbo.Sales", new[] { "ProductId" });
+            DropIndex("dbo.Sales", new[] { "SupermarketId" });
+            DropIndex("dbo.Products", new[] { "VendorId" });
+            DropIndex("dbo.Products", new[] { "MeasureId" });
+            DropTable("dbo.Supermarkets");
+            DropTable("dbo.Sales");
+            DropTable("dbo.Vendors");
+            DropTable("dbo.Products");
+            DropTable("dbo.Measures");
+        }
+    }
+}
