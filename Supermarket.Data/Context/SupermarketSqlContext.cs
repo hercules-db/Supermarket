7ab934960f5c22c4ea8c12ccddd3870a8ffc7658
Karim <unbelt@outlook.com>
2015-03-20 23:37:33 +0200

    Edit Context IP

--- Supermarket.Data/Context/SupermarketSqlContext.cs
+++ Supermarket.Data/Context/SupermarketSqlContext.cs
@@ -6,7 +6,7 @@
     public class SupermarketSqlContext : SupermarketContext
     {
         public const string ConnectionString =
-            "Server=.;Database=Supermarket;Persist Security Info=True;User ID=hercules;Password=pass123;";
+            "Server=62.176.105.57;Database=Supermarket;Persist Security Info=True;User ID=hercules;Password=pass123;";
 
         public SupermarketSqlContext()
             : base(new SqlConnection(ConnectionString))
