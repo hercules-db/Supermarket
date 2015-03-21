1fba295bd32c8706cf958a46a9efe6b89f08b0ac
SKoen <slavi.koen@abv.bg>
2015-03-21 16:57:25 +0200

    Fix some bug in pdf.cs, put some desing on pdf file

--- Supermarket.Data/Exports/Pdf.cs
+++ Supermarket.Data/Exports/Pdf.cs
@@ -18,42 +18,78 @@
         public static void Export(ISupermarketContext context, DateTime? startDate, DateTime? endDate)
         {
             var document = new Document(PageSize.LETTER, 10, 10, 42, 35);
-            var file =new FileStream(FilePath, FileMode.OpenOrCreate);
-            PdfWriter.GetInstance(document,file );
-
+            var file = new FileStream(FilePath, FileMode.OpenOrCreate);
+            PdfWriter.GetInstance(document, file);
             document.Open();
-            int numberOfColumn = 2;
+
+            int numberOfColumn = 5;
+
             var PdfTable = new PdfPTable(numberOfColumn);
+            PdfTable.HorizontalAlignment = 1;
             var cellHeader = new PdfPCell(new Phrase(DocumentHeaderTitle));
+
+            var productsData = (
+            from p in context.Products
+            group p by p.ProductName into gr
+            join s in context.Sales on gr.FirstOrDefault().ProductId equals s.ProductId into ps
+            join m in context.Supermarkets on ps.FirstOrDefault().SupermarketId equals m.SupermarketId
+            select new
+            {
+                productName = gr.FirstOrDefault().ProductName,
+                quantity = ps.FirstOrDefault().Quantity,
+                unitPrice = ps.FirstOrDefault().UnitPrice,
+                location = m.SupermarketName,
+                productSaleSum = ps.Sum(sum => sum.SaleSum)
+            }
+            );
+
+            //Add report Date
+            PdfTable.AddCell(new Phrase(endDate.ToString()));
+
+
             cellHeader.Colspan = numberOfColumn;
             cellHeader.HorizontalAlignment = 1;
             PdfTable.AddCell(cellHeader);
-            var data = (
-                        from m in context.Supermarkets
-                        group m by m.SupermarketName into g
-                        join s in context.Sales on g.FirstOrDefault().SupermarketId equals s.SupermarketId into sm
-                        select new
-                        {
-                            marketName = g.FirstOrDefault().SupermarketName,
-                            saleSum = sm.Sum(d => d.SaleSum)
-                        }
-                        );
+            
+            //Add labels
+            var productLabel = new PdfPCell(new Phrase("Product"));
+            var quantityLabel = new PdfPCell(new Phrase("Quantity"));
+            var unitPriceLabel = new PdfPCell(new Phrase("Unit Price"));
+            var locationlabel = new PdfPCell(new Phrase("Location"));
+            var sumLabel = new PdfPCell(new Phrase("Sum"));
 
-            //var data = context.Sales
-            //    .Join(context.Supermarkets,
-            //    sale => sale.SupermarketId,
-            //    market => market.SupermarketId,
-            //    (sale, market) => new { marketName = market.SupermarketName, saleSum = sale.SaleSum })
-            //    .GroupBy(g => g.marketName).Select(s => new {marketName = context., saleSum = sale.SaleSum });
-            foreach (var sale in data)
+            productLabel.HorizontalAlignment = 1;
+            quantityLabel.HorizontalAlignment = 1;
+            unitPriceLabel.HorizontalAlignment = 1;
+            locationlabel.HorizontalAlignment = 1;
+            sumLabel.HorizontalAlignment = 1;
+
+            productLabel.BackgroundColor = BaseColor.CYAN;
+            quantityLabel.BackgroundColor = BaseColor.CYAN;
+            unitPriceLabel.BackgroundColor = BaseColor.CYAN;
+            locationlabel.BackgroundColor = BaseColor.CYAN;
+            sumLabel.BackgroundColor = BaseColor.CYAN;
+
+            PdfTable.AddCell(productLabel);
+            PdfTable.AddCell(quantityLabel);
+            PdfTable.AddCell(unitPriceLabel);
+            PdfTable.AddCell(locationlabel);
+            PdfTable.AddCell(sumLabel);
+
+
+            foreach (var product in productsData)
             {
-      //          var cellSaleId = sale.saleId.ToString();
-                var cellSaleSum = sale.saleSum.ToString() + " лв.";
-                var cellSaleSupermarketName = sale.marketName.ToString();
+                var cellProductName = product.productName.ToString();
+                var cellQuantity = product.quantity.ToString();
+                var cellUnitPrice = product.unitPrice.ToString();
+                var cellLocation = product.location.ToString();
+                var cellProductSaleSum = product.productSaleSum.ToString();
 
-         //       PdfTable.AddCell(cellSaleId);
-                PdfTable.AddCell(cellSaleSupermarketName);
-                PdfTable.AddCell(cellSaleSum);
+                PdfTable.AddCell(cellProductName);
+                PdfTable.AddCell(cellQuantity);
+                PdfTable.AddCell(cellUnitPrice);
+                PdfTable.AddCell(cellLocation);
+                PdfTable.AddCell(cellProductSaleSum);
             }
             document.Add(PdfTable);
             document.Close();
