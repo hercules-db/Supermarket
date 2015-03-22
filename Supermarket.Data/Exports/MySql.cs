a6745dd0ace09eb1a1fedc39e7bbf4d46cc6aa70
Vasil Tsintsev <v.tsintsev@gmail.com>
2015-03-22 17:13:27 +0200

    Added MySQL support partially

--- Supermarket.Data/Exports/MySql.cs
+++ Supermarket.Data/Exports/MySql.cs
@@ -1,6 +1,20 @@
 ﻿namespace Supermarket.Data.Exports
 {
+    using System;
+    using System.Linq;
+    using Context;
+    using System.Text;
+
     public class MySql
     {
+
+        public static void Export(ISupermarketContext context, DateTime? startDate, DateTime? endDate)
+        {
+            //var sqlString = new StringBuilder();
+            //var vendors = context.Vendors.ToList();
+            //TODO: Implement
+            throw new NotImplementedException("MYSQL not implemented Yet");
+
+        }
     }
 }
